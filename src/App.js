365548740f05cfcd28906d0c9bf9ed8db3f7cc22
FLF1995 <carolfan17@163.com>
2019-12-04 16:30:24 +0800

    hello world

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return `${user.firstName} ${user.LastName}`;
+}
+const user = {
+  firstName: "Lufang",
+  LastName: "Fan"
+};
+const element = <h2>Hello {formatName(user)}</h2>;
+
+function Welcome(props) {
+return <h2>Hello, {props.name}</h2>
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,10 +22,11 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          {element}
+          <Welcome name="Sara" />
+          <Welcome name="gsm" />
+          <Welcome name="flf" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
