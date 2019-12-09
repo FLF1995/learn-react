80ebe9b33fd267788bc83b5291f2451bc1806d38
FLF1995 <carolfan17@163.com>
2019-12-09 16:57:23 +0800

    layout

---
+++ src/MainLayout.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class MainLayout extends Component {
+  render() {
+    return (
+      <div className="main-layout">
+        <div className="main-layout">
+					layout
+				</div>
+      </div>
+    );
+  }
+}
--- src/Router.js
+++ src/Router.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { HashRouter, Route, Switch, hashHistory } from "react-router-dom";
 import App from "./App";
 import Clock from "./Clock";
+import Layout from "./MainLayout";
 
-const BasicRoutes = ()=>(
-	<HashRouter history={hashHistory}>
+const BasicRoutes = () => (
+  <HashRouter history={hashHistory}>
+    <Layout></Layout>
     <Switch>
       <Route exact path="/app/:id" component={App}></Route>
       <Route exact path="/clock" component={Clock}></Route>
     </Switch>
-	</HashRouter>
-)
+  </HashRouter>
+);
 
 export default BasicRoutes;
